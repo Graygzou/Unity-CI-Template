8323eeae27c837734e271bdd6eb425e2e2050f3b
Graygzou <gregoire.boiron@gmail.com>
2018-06-28 14:22:34 -0400

    add dummy Unity project

---
+++ Assets/Editor/EditModePlayerTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerTest {
+
+	[Test]
+	public void CreatePlayerEmptyLifeTest() {
+        // Use the Assert class to test conditions.
+        PlayerScript player = new PlayerScript();
+        Assert.AreEqual(player.GetLife(), 3);
+    }
+
+    [Test]
+    public void CreatePlayerEmptyPointsTest()
+    {
+        PlayerScript player = new PlayerScript();
+        Assert.AreEqual(player.GetPoints(), 0);
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+	public IEnumerator PlayerTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿public class PlayerScript {
+
+    private int life;
+    private int points;
+
+    public PlayerScript() : this(3, 0) { }
+
+    public PlayerScript(int life, int points) {
+        this.life = life;
+        this.points = points;
+    }
+
+    public int GetLife() {
+        return this.life;
+    }
+
+    public int GetPoints() {
+        return this.points;
+    }
+
+}
