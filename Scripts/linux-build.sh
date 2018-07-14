148b7aa319694df017d2bdedde5e99b176694745
Graygzou <gregoire.boiron@gmail.com>
2018-07-14 13:40:42 +0200

    Rename build script, configure linux build, add first condition to HintPath.

--- Scripts/linux-build.sh
+++ Scripts/linux-build.sh
@@ -1,15 +1,10 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
 # Change this the name of your project. This will be the name of the final executables as well.
 project="Unity-CI-Template"
 
-ls -l /
-ls -l -R /Applications/
-
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/opt/Unity/Editor/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -20,7 +15,7 @@ echo "Attempting to build $project for Windows"
 echo "Windows return code: $?"
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/opt/Unity/Editor/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -31,7 +26,7 @@ echo "Attempting to build $project for OS X"
 echo "OS X return code: $?"
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/opt/Unity/Editor/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -42,6 +37,6 @@ echo "Attempting to build $project for Linux"
 echo "Linux return code: $?"
 
 echo 'Logs from build'
-cat $(pwd)/unity-osx.log
+cat $(pwd)/unity-linux.log
 
 set +e
